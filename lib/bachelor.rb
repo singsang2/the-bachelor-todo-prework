b8d1eaa0842f332f59a6f56dbc386e040e99fc09
Sung Bae <singsang2@gmail.com>
2018-10-10 17:16:08 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -34,10 +34,9 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  total_age = 0
+  total_age = 0.0
   data.fetch(season).each {|contestant| total_age += contestant.fetch("age".to_sym).to_i}
-  # average_age = total_age / data.fetch(season).length
-  data.fetch(season).length
+  average_age = total_age / data.fetch(season).length
 end
 
 data = {
